1fdacf26587e034f3cb8f6705ef01fd80adf069f
wangghb3 <151936132+wangghb3@users.noreply.github.com>
2024-05-06 08:47:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,6 @@ mv xray ${RELEASE_RANDOMNESS}
 cat config.json | base64 > config
 rm -f config.json
 
-# 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
-[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
-
 nginx
 base64 -d config > config.json
 ./${RELEASE_RANDOMNESS} -config=config.json
